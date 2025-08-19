862b8808785c6a620b746407dbd4796eb7a723b2
udengwu chukwudalu <praisedalu@gmail.com>
2025-08-19 15:12:31 +0100

    create a password verification password

--- security.py
+++ security.py
@@ -2,12 +2,26 @@
 """
 import os 
 salt =os.urandom(16)
-print("salt:", salt)
-print("Saltlength:", len(salt))
+# print("salt:", salt)
+# print("Saltlength:", len(salt))
 
 """Hashing a password with a salt"""
 import hashlib 
 password = b"Hello123"
 hashed= hashlib.pbkdf2_hmac("sha256",password,salt,100000)
-print("Hashed password:", hashed)
-print("Hashed length:", len(hashed))
+# print("Hashed password:", hashed)
+# print("Hashed length:", len(hashed))
+user_record = {
+    "salt":salt,
+    "hash": hashed,
+    "iterations": 100000
+}
+
+def password_verification(password,salt,hash, iterations):
+    real_password = hashlib.pbkdf2_hmac("sha256", password, salt, iterations)
+    if real_password == hash:
+        return True
+    else:
+        return False 
+    
+print(password_verification(password,user_record["salt"],user_record["hash"],user_record["iterations"]))
