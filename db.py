2a8466434461ce572e9ed3d16b7efdccd1ba51d3
udengwu chukwudalu <praisedalu@gmail.com>
2025-08-21 16:20:55 +0100

    create a connection object and add a cursor

---
+++ db.py
@@ -0,0 +1,36 @@
+import sqlite3
+import os 
+conn= sqlite3.connect("finance.db")
+c=conn.cursor()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# c.execute("SELECT sqlite_version();") #checking the version of sql
+# print(c.fetchone())# printing the version
