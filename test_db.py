fe28bf36087b95667beefe41c997a26771463be1
udengwu chukwudalu <praisedalu@gmail.com>
2025-09-09 11:21:54 +0100

    testing db

--- test_db.py
+++ test_db.py
@@ -1,2 +1,18 @@
-import db 
-print(db.get_user_email("alice@example.com"))
+
+import db
+import security
+
+# Ask user for sign-up details
+name = input("Enter your name: ")
+email = input("Enter your email: ")
+password = security.read_password("Create a password: ")
+
+if password is not None:
+    user_id = db.create_user(name, email, password)
+    if user_id:
+        print(f"User created! ID: {user_id}")
+    else:
+        print("❌Failed to create user.")
+else:
+    print("Signup cancelled (invalid password).")
+
