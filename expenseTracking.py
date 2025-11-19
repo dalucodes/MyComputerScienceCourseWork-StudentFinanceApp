840d05fc9786f704a5d0070a5f14310b56a36810
udengwu chukwudalu <praisedalu@gmail.com>
2025-11-19 13:18:13 +0000

    fix amount error and date error

--- expenseTracking.py
+++ expenseTracking.py
@@ -3,15 +3,24 @@ def addexpenses():
     while True:
         date_input= input("Enter a date YYYY-MM-DD")
         try:
-            date=datetime.strptime(date_input, "%Y-%m-%d").date
+            date=datetime.strptime(date_input, "%Y-%m-%d").date()
             break
         except ValueError:
             print("Input valid date in format YYYY-DD-MM example(2025-12-05)")
 
-    amount =int(input("How much did you spend today"))
+    
     while True:
+        try:
+            amount =int(input("How much did you spend today"))
+            if amount<0:
+                print("Amount cannot be less than 0")
+            else:
+                break
+        except ValueError:
+            print("Please enter a valid number")
+            print(ValueError)
         print("Amount can not be less than 0")
-        amount =int(input("ENTER AN AMOUNT GREATER THAN 0"))
+       
     category=input("Food, Bills etc")
     merchant = input("What is the name of the store")
     max_length = 100
