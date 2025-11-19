5b1d8ce164974fc0a474a672cd1fd6525d826473
udengwu chukwudalu <praisedalu@gmail.com>
2025-11-19 15:55:50 +0000

    create an expense table,create save_expense function and get_expense function

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 import sqlite3
 import os 
 import security
+import expenseTracking
 conn= sqlite3.connect("finance.db")
 c=conn.cursor()
 
@@ -15,18 +16,26 @@ c.execute(
 
     )""")
 conn.commit()
-c.execute("SELECT * FROM Users")
+c.execute(
+    """CREATE TABLE IF NOT EXISTS Expenses(
+        id INTEGER PRIMARY KEY AUTOINCREMENT ,
+        amount REAL NOT NULL,
+        category TEXT NOT NULL ,
+        merchant TEXT,
+        notes TEXT, 
+        date TEXT NOT NULL,
+        user_id INTEGER NOT NULL,
+        FOREIGN KEY(user_id) REFERENCES Users(id)
+
+    )""")
+conn.commit()
+# c.execute("SELECT * FROM Users")
 # print(c.fetchall())
-
-
 # c.execute(
 #     """INSERT INTO Users (name, email, password_hash, salt, iterations)
 #      VALUES("Alice", "alice@example.com", "hash123", "salt123", 100000)"""
- 
 # )
-
 # conn.commit()
-
 def get_user_email(email):
     email=email.strip().lower()
     query = "SELECT id, name, email, password_hash, salt, iterations FROM Users WHERE email = ?"
@@ -42,8 +51,6 @@ def get_user_email(email):
         "salt": row[4],
         "iterations": row[5]
     }
-
-
 def create_user_email(name, email, password):
     name_clean = name.strip()
     email_clean = email.strip().lower()
@@ -84,6 +91,39 @@ def verify_user(email, password):
         print("Access denied")
         return False
     
+def save_expenses(user_id, expense_record):
+    query= """
+        INSERT INTO Expenses(amount,category,merchant,notes,date,user_id)
+        VALUES(?,?,?,?,?,?)
+    """
+    c.execute(query,(
+        expense_record["amount"],
+        expense_record["category"],
+        expense_record["merchant"],
+        expense_record["notes"],
+        expense_record["date"].isoformat(),
+        user_id
+    ))
+    conn.commit()
+    return c.lastrowid
+
+def get_expenses(user_id):
+    query= "SELECT id, amount, category,merchant,notes,date FROM Expenses WHERE user_id =?"
+    c.execute(query,(user_id,))
+    rows = c.fetchall()
+    expenses =[]
+    for row in rows:
+        expenses.append({
+            "id":row[0],
+            "amount":row[1],
+            "category":row[2],
+            "merchant":row[3],
+            "notes":row[4],
+            "date":row[5]
+        })
+    return expenses
+
+
 
 
 
@@ -106,4 +146,4 @@ def verify_user(email, password):
 
 # c.execute("SELECT sqlite_version();") #checking the version of sql
 # print(c.fetchone())# printing the version
-15243566
+# 15243566
